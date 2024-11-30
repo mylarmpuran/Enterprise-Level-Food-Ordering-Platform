2e4246acee479199378522dff8f659ccd8f380e7
puran <mylarmpuran@gmail.com>
2024-11-30 19:02:24 +0530

    fdg

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import express,{ Request, Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoutes.ts"
+import myUserRoute from "./routes/MyUserRoutes"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("connected to database"))
 
