cf9e340516584a83d9fa0496b773a05613aad24f
puran <mylarmpuran@gmail.com>
2024-12-04 13:54:16 +0530

    form

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -5,7 +5,7 @@ import { Request, Response, NextFunction } from "express";
 const handleValidationErrors = async (
     req: Request,
     res: Response,
-    next: NextFunction
+    next: NextFunction,
 ) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
