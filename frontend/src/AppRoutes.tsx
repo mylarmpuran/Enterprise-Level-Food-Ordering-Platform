fb46d42dddd907e1c9ac35a5f5f37f9f662fd24e
puran <mylarmpuran@gmail.com>
2024-12-07 14:48:27 +0530

    atuh

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,16 +3,33 @@ import Layout from "./layouts/layout"
 import HomePage from "./pages/HomePage";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 const AppRoutes = () => {
     return (
-        <Routes>
-            <Route path='/auth-callback' element={<AuthCallbackPage/>}/>
-            <Route path='/' element={<Layout showHero><HomePage/></Layout>}/>
-            <Route path='/user-profile' element={<Layout><UserProfilePage/></Layout>}/>
-            <Route path='*' element={<Navigate to="/" />}/>
-        </Routes>
-    )
+      <Routes>
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route
+          path="/"
+          element={
+            <Layout showHero>
+              <HomePage />
+            </Layout>
+          }
+        />
+        <Route element={<ProtectedRoute/>}>
+          <Route
+            path="/user-profile"
+            element={
+              <Layout>
+                <UserProfilePage />
+              </Layout>
+            }
+          />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    );
 }
 
 export default AppRoutes;
