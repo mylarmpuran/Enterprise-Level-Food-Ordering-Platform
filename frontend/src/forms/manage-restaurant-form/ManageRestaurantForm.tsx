b4811bdaa2bac3dd527a15af54d7499f219a14fd
puran <mylarmpuran@gmail.com>
2024-12-11 23:15:31 +0530

    cuisine

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -5,6 +5,7 @@ import { z } from "zod";
 import DetailsSection from "./DetailsSection";
 import { Separator } from "@/components/ui/separator";
 import CuisinesSection from "./CuisinesSection";
+import MenuSection from "./MenuSection";
 
 const formSchema = z.object({
     restaurantName: z.string({
@@ -64,6 +65,8 @@ const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
         <DetailsSection/>
         <Separator/>
         <CuisinesSection/>
+        <Separator/>
+        <MenuSection/>
       </form>
     </Form>
   );
