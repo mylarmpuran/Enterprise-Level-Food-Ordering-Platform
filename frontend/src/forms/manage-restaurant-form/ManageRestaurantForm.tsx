8f04cb493ca671f5dddb386fa35126cac6edae74
puran <mylarmpuran@gmail.com>
2024-12-11 21:17:45 +0530

    cuisecheckbox

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
+import CuisinesSection from "./CuisinesSection";
 
 const formSchema = z.object({
     restaurantName: z.string({
@@ -58,9 +60,10 @@ const ManageRestaurantForm = ({onSave, isLoading}: Props) => {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8 bg-gray-50 p-10 rounded-lg"
-      >
+        className="space-y-8 bg-gray-50 p-10 rounded-lg" >
         <DetailsSection/>
+        <Separator/>
+        <CuisinesSection/>
       </form>
     </Form>
   );
